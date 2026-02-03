f6d1d8d2ac1e409efbfc8424b400ae1a340bfd17
tsagueyves21 <tsagueyvesthibaut@gmail.com>
2026-02-03 15:40:07 +0100

    sff

--- src/valentine-api/src/main/java/com/valentine/valentine/controller/ValentineController.java
+++ src/valentine-api/src/main/java/com/valentine/valentine/controller/ValentineController.java
@@ -19,7 +19,6 @@ import java.util.Map;
 @RequestMapping("/api/valentine")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin(origins = "*", maxAge = 3600)
 public class ValentineController {
 
     private final HttpNotificationSender notificationSender;
